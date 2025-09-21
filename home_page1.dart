0191362ed69243a6533edde3cf59e740d164887d
noha marwan <noha@1234>
2025-09-21 22:10:20 +0300

    add      حجم الخط للنص

--- home_page1.dart
+++ home_page1.dart
@@ -72,7 +72,7 @@ class _HomePageState extends State<HomePage> {
             const SizedBox(height: 10),
             Text(
               networkStatus,
-//               style: const TextStyle(fontSize: 18), // ✅ حجم الخط للنص
+              style: const TextStyle(fontSize: 18), 
 //             ),
 //           ],
 //         ),
