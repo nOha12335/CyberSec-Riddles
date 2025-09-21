6dd25fa079a3dfc5674e2b9d5f141791899da882
noha marwan <noha@1234>
2025-09-21 22:50:39 +0300

    add     مكتبة لتخزين البيانات بأمان

--- db/riddle_page1.dart
+++ db/riddle_page1.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:crypto/crypto.dart';
-// import 'package:flutter_secure_storage/flutter_secure_storage.dart'; // ✅ مكتبة لتخزين البيانات بأمان
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 // import 'dart:convert'; // ✅ لتحويل النصوص إلى bytes للتشفير
 //
 // class RiddlePage extends StatefulWidget {
